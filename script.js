475b472117fa799fc0350684da978ba2e2375acc
ocampomarkchristian48-ctrl <ocampomarkchristian48@gmail.com>
2026-03-19 22:42:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function startGame(){
 
+let name=document.getElementById("playerName").value.trim();
+
+if(name===""){
+alert("Enter your name first!");
+return;
+}
+
+// hide start
+document.getElementById("startScreen").classList.add("hidden");
+
+// show description
+document.getElementById("descScreen").classList.remove("hidden");
+}
+
+// 👉 SHOW TOPICS (4 BOX)
+function showTopics(){
+
+document.getElementById("descScreen").classList.add("hidden");
+document.getElementById("topicScreen").classList.remove("hidden");
+
+}
